f6d177115357dcbc466db89abe2e5ff49a5a5291
benkywenk <77287657+benkywenk@users.noreply.github.com>
2022-02-28 13:37:59 +0000

    Updated to use logger

--- scripts/merge.py
+++ scripts/merge.py
@@ -7,24 +7,25 @@ import shutil
 from glob import glob
 import time
 
+import logging
+logger = logging.getLogger(__name__)
+
 def merge(data_dir):
 
-    print('Merging files...')
+    logger.info('Merging files...')
     t0 = time.time()
 
     analysis_dir = path.join(data_dir, "analysis")
     post.merge_process_files(analysis_dir, cleanup=True)
-    set_paths = glob(path.join(analysis_dir, '*.h5'))
-    post.merge_sets(path.join(data_dir, 'analysis_new.h5'), set_paths, cleanup=True)
-    shutil.move(path.join(data_dir, 'analysis_new.h5'), path.join(data_dir, 'analysis.h5'))
+    set_paths = glob(path.join(analysis_dir, 'analysis_s*.h5'))
+    post.merge_sets(path.join(data_dir, 'analysis.h5'), set_paths, cleanup=True)
 
     state_dir = path.join(data_dir, "state")
     post.merge_process_files(state_dir, cleanup=True)
-    set_paths = glob(path.join(state_dir, '*.h5'))
-    post.merge_sets(path.join(data_dir, 'state_new.h5'), set_paths, cleanup=True)
-    shutil.move(path.join(data_dir, 'state_new.h5'), path.join(data_dir, 'state.h5'))
+    set_paths = glob(path.join(state_dir, 'state_s*.h5'))
+    post.merge_sets(path.join(data_dir, 'state.h5'), set_paths, cleanup=True)
 
-    print('Finished merging files, took {} seconds'.format(time.time() - t0))
+    logger.info('Finished merging files, took {} seconds'.format(time.time() - t0))
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
