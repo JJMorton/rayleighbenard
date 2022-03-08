Fixed gradient function calls

scripts/plot.py
@@ -392,15 +392,15 @@ def plot_momentum_terms_post(data_dir, plot_dir):
         # The x component terms
         print("  Calculating x terms...")
         coeff = np.sin(params["Theta"]) * params["Ta"]**0.5
-        viscous_x = -np.gradient(np.gradient(u, z, edge_order=2)[-1], z, edge_order=2)[-1] / coeff
+        viscous_x = -np.gradient(np.gradient(u, z, axis=-1, edge_order=2), z, axis=-1, edge_order=2) / coeff
         coriolis_x = -v
-        stress_x = np.mean(np.gradient(average_horizontal( (u - np.mean(u, axis=0, keepdims=True)) * (w - np.mean(w, axis=0, keepdims=True)) ), z, edge_order=2)[-1], axis=0) / coeff
+        stress_x = np.mean(np.gradient(average_horizontal( (u - np.mean(u, axis=0, keepdims=True)) * (w - np.mean(w, axis=0, keepdims=True)) ), z, axis=-1, edge_order=2), axis=0) / coeff
 
         # The y component terms
         print("  Calculating y terms...")
-        viscous_y = -np.gradient(np.gradient(v, z, edge_order=2)[-1], z, edge_order=2)[-1] / coeff
+        viscous_y = -np.gradient(np.gradient(v, z, axis=-1, edge_order=2), z, axis=-1, edge_order=2) / coeff
         coriolis_y = u
-        stress_y = np.mean(np.gradient(average_horizontal( (v - np.mean(v, axis=0, keepdims=True)) * (w - np.mean(w, axis=0, keepdims=True)) ), z, edge_order=2)[-1], axis=0) / coeff
+        stress_y = np.mean(np.gradient(average_horizontal( (v - np.mean(v, axis=0, keepdims=True)) * (w - np.mean(w, axis=0, keepdims=True)) ), z, axis=-1, edge_order=2), axis=0) / coeff
 
         # Averaging in time and horizontally in space...
         print("  Averaging...")
@@ -503,27 +503,27 @@ def plot_momentum_terms_filtered(data_dir, plot_dir):
     # The x component terms
     print("  Calculating x terms...")
     coeff = np.sin(params["Theta"]) * params["Ta"]**0.5
-    viscous_x = -np.gradient(np.gradient(u, z, edge_order=2)[-1], z, edge_order=2)[-1] / coeff
-    viscous_x_low = -np.gradient(np.gradient(u_low, z, edge_order=2)[-1], z, edge_order=2)[-1] / coeff
-    viscous_x_high = -np.gradient(np.gradient(u_high, z, edge_order=2)[-1], z, edge_order=2)[-1] / coeff
+    viscous_x = -np.gradient(np.gradient(u, z, axis=-1, edge_order=2), z, axis=-1, edge_order=2) / coeff
+    viscous_x_low = -np.gradient(np.gradient(u_low, z, axis=-1, edge_order=2), z, axis=-1, edge_order=2) / coeff
+    viscous_x_high = -np.gradient(np.gradient(u_high, z, axis=-1, edge_order=2), z, axis=-1, edge_order=2) / coeff
     stress_x = np.mean(np.gradient(average_horizontal(
         (u - np.mean(u, axis=0, keepdims=True)) * (w - np.mean(w, axis=0, keepdims=True))
-    ), z, edge_order=2)[-1], axis=0) / coeff
+    ), z, axis=-1, edge_order=2), axis=0) / coeff
     stress_x_low = np.mean(np.gradient(average_horizontal(
         (u_low - np.mean(u_low, axis=0, keepdims=True)) * (w_low - np.mean(w_low, axis=0, keepdims=True))
-    ), z, edge_order=2)[-1], axis=0) / coeff
+    ), z, axis=-1, edge_order=2), axis=0) / coeff
     stress_x_high = np.mean(np.gradient(average_horizontal(
         (u_high - np.mean(u_high, axis=0, keepdims=True)) * (w_high - np.mean(w_high, axis=0, keepdims=True))
-    ), z, edge_order=2)[-1], axis=0) / coeff
+    ), z, axis=-1, edge_order=2), axis=0) / coeff
 
     # The y component terms
     print("  Calculating y terms...")
-    viscous_y = -np.gradient(np.gradient(v, z, edge_order=2)[-1], z, edge_order=2)[-1] / coeff
-    viscous_y_low = -np.gradient(np.gradient(v_low, z, edge_order=2)[-1], z, edge_order=2)[-1] / coeff
-    viscous_y_high = -np.gradient(np.gradient(v_high, z, edge_order=2)[-1], z, edge_order=2)[-1] / coeff
-    stress_y = np.mean(np.gradient(average_horizontal( (v - np.mean(v, axis=0, keepdims=True)) * (w - np.mean(w, axis=0, keepdims=True)) ), z, edge_order=2)[-1], axis=0) / coeff
-    stress_y_low = np.mean(np.gradient(average_horizontal( (v_low - np.mean(v_low, axis=0, keepdims=True)) * (w_low - np.mean(w_low, axis=0, keepdims=True)) ), z, edge_order=2)[-1], axis=0) / coeff
-    stress_y_high = np.mean(np.gradient(average_horizontal( (v_high - np.mean(v_high, axis=0, keepdims=True)) * (w_high - np.mean(w_high, axis=0, keepdims=True)) ), z, edge_order=2)[-1], axis=0) / coeff
+    viscous_y = -np.gradient(np.gradient(v, z, axis=-1, edge_order=2), z, axis=-1, edge_order=2) / coeff
+    viscous_y_low = -np.gradient(np.gradient(v_low, z, axis=-1, edge_order=2), z, axis=-1, edge_order=2) / coeff
+    viscous_y_high = -np.gradient(np.gradient(v_high, z, axis=-1, edge_order=2), z, axis=-1, edge_order=2) / coeff
+    stress_y = np.mean(np.gradient(average_horizontal( (v - np.mean(v, axis=0, keepdims=True)) * (w - np.mean(w, axis=0, keepdims=True)) ), z, axis=-1, edge_order=2), axis=0) / coeff
+    stress_y_low = np.mean(np.gradient(average_horizontal( (v_low - np.mean(v_low, axis=0, keepdims=True)) * (w_low - np.mean(w_low, axis=0, keepdims=True)) ), z, axis=-1, edge_order=2), axis=0) / coeff
+    stress_y_high = np.mean(np.gradient(average_horizontal( (v_high - np.mean(v_high, axis=0, keepdims=True)) * (w_high - np.mean(w_high, axis=0, keepdims=True)) ), z, axis=-1, edge_order=2), axis=0) / coeff
 
     # Averaging in time and horizontally in space...
     print("  Averaging...")
@@ -619,7 +619,7 @@ if __name__ == "__main__":
         pass
 
     plot_velocities(data_dir, plot_dir)
-    plot_temperature(data_dir, plot_dir)
+    # plot_temperature(data_dir, plot_dir)
     plot_heat_flux_z(data_dir, plot_dir)
     plot_energy(data_dir, plot_dir)
     plot_velocity_filters(data_dir, plot_dir)