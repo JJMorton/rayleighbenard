423235ac6a4898eb9dc86887491d8a7e6d738871
JJMorton <jjmorton1636@gmail.com>
2022-04-19 21:12:18 +0100

    Update merge.py to merge previous run with the new one

--- scripts/merge.py
+++ scripts/merge.py
@@ -16,18 +16,36 @@ def merge(data_dir):
     t0 = time.time()
 
     analysis_dir = path.join(data_dir, "analysis")
-    post.merge_process_files(analysis_dir, cleanup=True)
+    analysis_file = path.join(data_dir, 'analysis.h5')
+    post.merge_process_files(analysis_dir, cleanup=False)
     set_paths = glob(path.join(analysis_dir, 'analysis_s*.h5'))
+    # If analysis.h5 already exists, merge it with all the new data
+    if path.exists(analysis_file):
+        old_file = path.join(data_dir, 'analysis_old.h5')
+        shutil.move(analysis_file, old_file)
+        set_paths.append(old_file)
     post.merge_sets(path.join(data_dir, 'analysis.h5'), set_paths, cleanup=True)
 
     state_dir = path.join(data_dir, "state")
-    post.merge_process_files(state_dir, cleanup=True)
+    state_file = path.join(data_dir, 'state.h5')
+    post.merge_process_files(state_dir, cleanup=False)
     set_paths = glob(path.join(state_dir, 'state_s*.h5'))
+    # If state.h5 already exists, merge it with all the new data
+    if path.exists(state_file):
+        old_file = path.join(data_dir, 'state_old.h5')
+        shutil.move(state_file, old_file)
+        set_paths.append(old_file)
     post.merge_sets(path.join(data_dir, 'state.h5'), set_paths, cleanup=True)
     
     vel_dir = path.join(data_dir, "vel")
-    post.merge_process_files(vel_dir, cleanup=True)
+    vel_file = path.join(data_dir, 'vel.h5')
+    post.merge_process_files(vel_dir, cleanup=False)
     set_paths = glob(path.join(vel_dir, 'vel_s*.h5'))
+    # If vel.h5 already exists, merge it with all the new data
+    if path.exists(vel_file):
+        old_file = path.join(data_dir, 'vel_old.h5')
+        shutil.move(vel_file, old_file)
+        set_paths.append(old_file)
     post.merge_sets(path.join(data_dir, 'vel.h5'), set_paths, cleanup=True)
 
     logger.info('Finished merging files, took {} seconds'.format(time.time() - t0))
